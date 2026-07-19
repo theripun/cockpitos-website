aadd34b66dab4d230af586b98763ac8ce7ba1dc5
ripunstack <ripunbasumatary04@gmail.com>
2026-07-19 08:44:20 +0000

    Normalize env style agent install URLs

--- lib/install-command.ts
+++ lib/install-command.ts
@@ -3,5 +3,6 @@ import { BASE_URL } from "@/lib/baseURL";
 export function normalizeAgentInstallCommand(command: string) {
     return command
         .replace(/https?:\/\/[^\s]+\/cocktail\/install\.sh/g, `${BASE_URL}/cocktail/install.sh`)
+        .replace(/SERVER_URL=(['"])https?:\/\/[^'"\s]+\1/g, `SERVER_URL='${BASE_URL}'`)
         .replace(/--serverUrl\s+https?:\/\/[^\s]+/g, `--serverUrl ${BASE_URL}`);
 }
